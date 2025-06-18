51e9006b522eb059f2ca7f10ce2515c2010a4895
spencermcarthur <55674059+spencermcarthur@users.noreply.github.com>
2025-06-18 16:14:38 -0500

    Move SemLock impl

--- include/SemLock.hpp
+++ include/SemLock.hpp
@@ -1,100 +1,35 @@
 #pragma once
 
-#include <fcntl.h>
 #include <linux/limits.h>
 #include <semaphore.h>
 
-#include <cerrno>
-#include <cstring>
-#include <format>
-#include <iostream>
-#include <stdexcept>
+#include <atomic>
+#include <cstddef>
 #include <string_view>
 
 #include "Defines.hpp"
 
-constexpr int OPEN_MODE_RDWR = 0600;
-
 // Semaphore lock class
 class SemLock {
+    static constexpr int OPEN_MODE_RDWR = 0600;
+
 public:
     // See "DESCRIPTION/Named semaphores" at
     // https://man7.org/linux/man-pages/man7/sem_overview.7.html
     static constexpr size_t MAX_SEM_NAME_LEN = NAME_MAX - 4;
 
-    explicit SemLock(std::string_view name) {
-        // Validate name
-        if (name.empty() || name.length() > MAX_SEM_NAME_LEN) {
-            throw std::length_error(std::format(
-                "({}:{}) Semaphore name \"{}\" of length {} is invalid: "
-                "max name length for a semaphore is {}",
-                __FILE__, __LINE__, name, name.size(), MAX_SEM_NAME_LEN));
-        }
-
-        // Copy name
-        m_Name = new char[name.size() + 1]{};
-        std::strncpy(m_Name, name.data(), name.size());
-
-        // Try to create the semaphore
-        sem_t *sem = sem_open(m_Name, O_CREAT | O_EXCL, OPEN_MODE_RDWR, 1);
-        if (sem == SEM_FAILED) {
-            // Failed
-            if (errno == EEXIST) {
-                // Already existed - just open it
-                sem = sem_open(m_Name, 0, OPEN_MODE_RDWR);
-            }
-
-            if (sem == SEM_FAILED) {
-                // Failed again
-                const int err = errno;
-                throw std::runtime_error(
-                    std::format("({}:{}) Failed to open semaphore {}: {}",
-                                __FILE__, __LINE__, m_Name, strerror(err)));
-            }
-        }
-
-        m_Sem = sem;
-    }
-
-    ~SemLock() {
-        const int ret = sem_close(m_Sem);
-        if (ret == -1) {
-            // Failed - print error
-            const int err = errno;
-            std::cerr << std::format(
-                "({}:{}) Failed to close semaphore {}: {}\n", __FILE__,
-                __LINE__, m_Name, strerror(err));
-        }
-
-        delete[] m_Name;
-    }
+    explicit SemLock(std::string_view name);
+    ~SemLock();
 
     EXPLICIT_DELETE_CONSTRUCTORS(SemLock);
 
-    bool Acquire() { return !(sem_trywait(m_Sem) == -1); }
-    bool Release() { return !(sem_post(m_Sem) == -1); }
-
-    bool Acquire(int &errc) {
-        const int ret = sem_trywait(m_Sem);
-        if (ret == -1) {
-            errc = errno;
-            return false;
-        }
-
-        return true;
-    }
-
-    bool Release(int &errc) {
-        const int ret = sem_post(m_Sem);
-        if (ret == -1) {
-            errc = errno;
-            return false;
-        }
-
-        return true;
-    }
+    bool Acquire() noexcept;
+    bool Acquire(int &err) noexcept;
+    bool Release() noexcept;
+    bool Release(int &err) noexcept;
 
 private:
     char *m_Name{nullptr};
     sem_t *m_Sem{nullptr};
+    std::atomic_bool m_HoldsOwnership{false};
 };
---
+++ src/SemLock.cpp
@@ -0,0 +1,107 @@
+#include "SemLock.hpp"
+
+#include <fcntl.h>
+#include <semaphore.h>
+
+#include <atomic>
+#include <cerrno>
+#include <cstring>
+#include <format>
+#include <iostream>
+#include <stdexcept>
+#include <string_view>
+
+SemLock::SemLock(std::string_view name) {
+    // Validate name
+    if (name.empty() || name.length() > MAX_SEM_NAME_LEN) {
+        throw std::length_error(std::format(
+            "({}:{}) Semaphore name \"{}\" of length {} is invalid: "
+            "max name length for a semaphore is {}",
+            __FILE__, __LINE__, name, name.size(), MAX_SEM_NAME_LEN));
+    }
+
+    // Copy name
+    m_Name = new char[name.size() + 1]{};
+    std::strncpy(m_Name, name.data(), name.size());
+
+    // Try to create the semaphore
+    sem_t *sem = sem_open(m_Name, O_CREAT | O_EXCL, OPEN_MODE_RDWR, 1);
+    if (sem == SEM_FAILED) {
+        // Failed
+        if (errno == EEXIST) {
+            // Already existed - just open it
+            sem = sem_open(m_Name, 0, OPEN_MODE_RDWR);
+        }
+
+        if (sem == SEM_FAILED) {
+            // Failed again
+            const int err = errno;
+            throw std::runtime_error(
+                std::format("({}:{}) Failed to open semaphore {}: {}", __FILE__,
+                            __LINE__, m_Name, strerror(err)));
+        }
+    }
+
+    m_Sem = sem;
+}
+
+SemLock::~SemLock() {
+    if (m_HoldsOwnership.load(std::memory_order_acquire)) {
+        int err;
+        if (!Release(err)) {
+            std::cerr << std::format(
+                "({}:{}) Failed to release smaphore {}: {}", __FILE__, __LINE__,
+                m_Name, strerror(err));
+        }
+    }
+
+    const int ret = sem_close(m_Sem);
+    if (ret == -1) {
+        // Failed - print error
+        const int err = errno;
+        std::cerr << std::format("({}:{}) Failed to close semaphore {}: {}\n",
+                                 __FILE__, __LINE__, m_Name, strerror(err));
+    }
+
+    delete[] m_Name;
+}
+
+bool SemLock::Acquire() noexcept {
+    const bool acquired = sem_trywait(m_Sem) == 0;
+    if (acquired) {
+        m_HoldsOwnership.store(true, std::memory_order_release);
+    }
+
+    return acquired;
+}
+
+bool SemLock::Release() noexcept {
+    const bool released = sem_post(m_Sem) == 0;
+    if (released) {
+        m_HoldsOwnership.store(false, std::memory_order_release);
+    }
+
+    return released;
+}
+
+bool SemLock::Acquire(int &err) noexcept {
+    const bool acquired = sem_trywait(m_Sem) == 0;
+    if (acquired) {
+        m_HoldsOwnership.store(true, std::memory_order_release);
+    } else {
+        err = errno;
+    }
+
+    return acquired;
+}
+
+bool SemLock::Release(int &err) noexcept {
+    const bool released = sem_post(m_Sem) == 0;
+    if (released) {
+        m_HoldsOwnership.store(false, std::memory_order_release);
+    } else {
+        err = errno;
+    }
+
+    return true;
+}
